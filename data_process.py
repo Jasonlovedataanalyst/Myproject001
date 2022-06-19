be12fa869d0142b1d40545836ecbcd97a6f62808
jason <jasonzhang@DESKTOP-FG2UAJV.localdomain>
2022-06-19 23:05:46 +0930

    dev 1

--- data_process.py
+++ data_process.py
@@ -1,11 +1,6 @@
 import os 
-<<<<<<< HEAD
 import json
 print('This is the change from dev2')
-print('this is the change from etl1')
 print('this is data process')
-=======
 imort json
-print('This is the change from etl1 asdsa')
-print ('this is data process')
->>>>>>> etl1
+
